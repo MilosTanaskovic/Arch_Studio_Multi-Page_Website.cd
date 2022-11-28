cd1de6c9ee13ad128109c31712b3ead993d30c74
Milos Tanaskovic <milostanaskovic.dev.data@gmail.com>
2022-11-28 16:30:49 +0100

    Portfolio page

--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -1,10 +1,104 @@
 import React from 'react';
+import { Body, HeadingS } from '../components/atoms/typography';
+import { Product } from '../components/moleculs';
+import { Products } from '../components/organisms';
 import { Layout } from '../templates';
 
 const Portfolio = () => {
     return (
         <Layout>
-            Portfolio Page
+             {/** Products Section(Prosucts) */}
+             <Products>
+                <Product
+                    bgImage={'https://s3.us-east-1.amazonaws.com/sectigo-sites-web/global/images/Sectigo/blog/Website-Monitoring-6-3-21-1.jpeg'}
+                >
+                    <HeadingS
+                        whiteColor
+                    >
+                        Project One
+                    </HeadingS>
+                    <Body
+                        whiteColor
+                    >
+                        Description
+                    </Body>
+                </Product>
+
+                <Product
+                    bgImage={'https://s3.us-east-1.amazonaws.com/sectigo-sites-web/global/images/Sectigo/blog/Website-Monitoring-6-3-21-1.jpeg'}
+                >
+                    <HeadingS
+                        whiteColor
+                    >
+                        Project One
+                    </HeadingS>
+                    <Body
+                        whiteColor
+                    >
+                        Description
+                    </Body>
+                </Product>
+
+                <Product
+                bgImage={'https://s3.us-east-1.amazonaws.com/sectigo-sites-web/global/images/Sectigo/blog/Website-Monitoring-6-3-21-1.jpeg'}
+                >
+                    <HeadingS
+                        whiteColor
+                    >
+                        Project One
+                    </HeadingS>
+                    <Body
+                        whiteColor
+                    >
+                        Description
+                    </Body>
+                </Product>
+                <Product
+                bgImage={'https://s3.us-east-1.amazonaws.com/sectigo-sites-web/global/images/Sectigo/blog/Website-Monitoring-6-3-21-1.jpeg'}
+            >
+                <HeadingS
+                    whiteColor
+                >
+                    Project One
+                </HeadingS>
+                <Body
+                    whiteColor
+                >
+                    Description
+                </Body>
+            </Product>
+
+            <Product
+                bgImage={'https://s3.us-east-1.amazonaws.com/sectigo-sites-web/global/images/Sectigo/blog/Website-Monitoring-6-3-21-1.jpeg'}
+            >
+                <HeadingS
+                    whiteColor
+                >
+                    Project One
+                </HeadingS>
+                <Body
+                    whiteColor
+                >
+                    Description
+                </Body>
+            </Product>
+
+            <Product
+            bgImage={'https://s3.us-east-1.amazonaws.com/sectigo-sites-web/global/images/Sectigo/blog/Website-Monitoring-6-3-21-1.jpeg'}
+            >
+                <HeadingS
+                    whiteColor
+                >
+                    Project One
+                </HeadingS>
+                <Body
+                    whiteColor
+                >
+                    Description
+                </Body>
+            </Product>
+                
+            </Products>
         </Layout>
     )
 }
